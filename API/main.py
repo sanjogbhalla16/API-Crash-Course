ccd723ee5a49dab6e7d929f9af1a1e8fe4fa99ad
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-02-10 11:28:16 +0530

    Changes

--- API/main.py
+++ API/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
